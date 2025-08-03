ec9afce2b7f96a405d6a3bbcbd426e1649a365c4
i-m-rajgupta <neeleshgupta32154@gmail.com>
2025-08-03 17:56:20 +0530

    Update search in app.js

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const ejsMate = require("ejs-mate");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const passportLocalMongoose = require("passport-local-mongoose");
+const Listing = require("./models/listing.js");
 
 const listingRouter = require("./routes/lisitng.js");
 const reviewRouter = require("./routes/review.js");
@@ -91,6 +92,32 @@ app.use((req,res,next)=>{
 app.get("/",(req,res)=>{
    res.redirect("/listings");
 });
+
+app.post("/search",async(req,res,next)=>{
+    const query = req.body.search;
+    if(!query){
+        res.redirect("/listings")
+    }
+      try {
+    const allListings = await Listing.find({
+          $or: [
+             { country: { $regex: query, $options: "i" } },
+               { location: { $regex: query, $options: "i" } },
+        { title : { $regex: query, $options: "i" } },
+      ]
+    })
+    if(allListings.length>0){
+     res.render("./listings/index.ejs",{allListings});
+    }else{
+        req.flash("error","No Results found for this query !!");
+        res.redirect("/listings");
+    }
+} catch (err) {
+   req.flash("error","No Results found for this query !!");
+   res.redirect("/listings");
+  }
+});
+
 app.use("/listings",listingRouter);
 app.use("/listings/:id/reviews",reviewRouter);
 app.use("/",userRouter);
